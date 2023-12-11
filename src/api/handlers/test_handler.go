901dd73abba0d8562e4248ba7591467ff0707f80
alireza-fa <alirezafeyze44@gmail.com>
2023-12-11 12:57:03 +0330

    test gin binder

--- src/api/handlers/test_handler.go
+++ src/api/handlers/test_handler.go
@@ -5,6 +5,16 @@ import (
 	"net/http"
 )
 
+type header struct {
+	Browser string
+	Id      string
+}
+
+type personData struct {
+	FirstName string
+	LastName  string
+}
+
 type TestHandler struct {
 }
 
@@ -39,5 +49,87 @@ func (h *TestHandler) UserByUsername(c *gin.Context) {
 func (h *TestHandler) CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"detail": "Created User",
+	})
+}
+
+func (h *TestHandler) HeaderBinder1(c *gin.Context) {
+	id := c.GetHeader("id")
+	c.JSON(http.StatusOK, gin.H{
+		"id":     id,
+		"result": "BinderHeader1",
+	})
+}
+
+func (h *TestHandler) HeaderBinder2(c *gin.Context) {
+	header := header{}
+	_ = c.BindHeader(&header)
+	c.JSON(http.StatusOK, gin.H{
+		"header": header,
+		"result": "BinderHeader2",
+	})
+}
+
+func (h *TestHandler) QueryBinder1(c *gin.Context) {
+	id := c.Query("id")
+	name := c.Query("name")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":     id,
+		"name":   name,
+		"result": "BinderQuery1",
+	})
+}
+
+func (h *TestHandler) QueryBinder2(c *gin.Context) {
+	ids := c.QueryArray("id")
+	name := c.Query("name")
+
+	c.JSON(http.StatusOK, gin.H{
+		"ids":    ids,
+		"name":   name,
+		"result": "BinderQuery2",
+	})
+}
+
+func (h *TestHandler) UriBinder(c *gin.Context) {
+	id := c.Param("id")
+	name := c.Param("name")
+	c.JSON(http.StatusOK, gin.H{
+		"id":     id,
+		"name":   name,
+		"result": "UriBinder",
+	})
+}
+
+func (h *TestHandler) BodyBinder(c *gin.Context) {
+	p := personData{}
+	c.ShouldBindJSON(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"person": p,
+		"result": "Body binder with ShouldBindJson",
+	})
+}
+
+func (h *TestHandler) FormBinder(c *gin.Context) {
+	p := personData{}
+	c.ShouldBind(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"person": p,
+		"result": "formBinder",
+	})
+}
+
+func (h *TestHandler) FileBinder(c *gin.Context) {
+	file, _ := c.FormFile("file")
+	err := c.SaveUploadedFile(file, "file")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"err": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"filename": file.Filename,
+		"result":   "FileBinder",
 	})
 }
--- src/api/routers/test_router.go
+++ src/api/routers/test_router.go
@@ -12,4 +12,18 @@ func TestRouter(r *gin.RouterGroup) {
 	r.POST("/users", handler.CreateUser)
 	r.GET("/users/:id", handler.UserById)
 	r.GET("/users/:id/:username", handler.UserByUsername)
+
+	r.POST("/binder/header1", handler.HeaderBinder1)
+	r.POST("/binder/header2", handler.HeaderBinder2)
+
+	r.POST("/binder/query1", handler.QueryBinder1)
+	r.POST("/binder/query2", handler.QueryBinder2)
+
+	r.POST("/binder/uri/:id/:name", handler.UriBinder)
+
+	r.POST("/binder/body", handler.BodyBinder)
+
+	r.POST("/binder/form", handler.FormBinder)
+
+	r.POST("/binder/file", handler.FileBinder)
 }
