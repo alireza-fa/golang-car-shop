72a4b1f4c0b8c48581dc689fdedeaf551fa1bdf1
alireza-fa <alirezafeyze44@gmail.com>
2023-12-11 21:24:36 +0330

    put the iranian mobile number validate to common

--- src/api/validations/mobile.go
+++ src/api/validations/mobile.go
@@ -1,9 +1,8 @@
 package validations
 
 import (
+	"github.com/alireza-fa/golang-car-shop/common"
 	"github.com/go-playground/validator/v10"
-	"log"
-	"regexp"
 )
 
 func IranianMobileNumberValidator(fld validator.FieldLevel) bool {
@@ -12,9 +11,5 @@ func IranianMobileNumberValidator(fld validator.FieldLevel) bool {
 		return false
 	}
 
-	res, err := regexp.MatchString(`^09(1[0-9]|2[0-2]|3[0-9]|9[0-9])[0-9]{7}$`, value)
-	if err != nil {
-		log.Print(err.Error())
-	}
-	return res
+	return common.IranianMobileNumberValidate(value)
 }
