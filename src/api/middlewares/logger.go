17ff0cd6ba2920104e034ae58584c85ad24a2632
alireza-fa <alirezafeyze44@gmail.com>
2023-12-17 00:10:42 +0330

    ignore swager api for logging

--- src/api/middlewares/logger.go
+++ src/api/middlewares/logger.go
@@ -6,6 +6,7 @@ import (
 	"github.com/alireza-fa/golang-car-shop/pkg/logging"
 	"github.com/gin-gonic/gin"
 	"io"
+	"strings"
 	"time"
 )
 
@@ -31,43 +32,47 @@ func DefaultStructuredLogger(cfg *config.Config) gin.HandlerFunc {
 
 func structuredLogger(logger logging.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		blw := &BodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
-		start := time.Now() // start
-		path := c.FullPath()
-		raw := c.Request.URL.RawQuery
+		if strings.Contains(c.FullPath(), "swagger") {
+			c.Next()
+		} else {
+			blw := &BodyLogWriter{body: bytes.NewBufferString(""), ResponseWriter: c.Writer}
+			start := time.Now() // start
+			path := c.FullPath()
+			raw := c.Request.URL.RawQuery
 
-		bodyBytes, _ := io.ReadAll(c.Request.Body)
-		c.Request.Body.Close()
-		c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+			bodyBytes, _ := io.ReadAll(c.Request.Body)
+			c.Request.Body.Close()
+			c.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
-		c.Writer = blw
-		c.Next()
+			c.Writer = blw
+			c.Next()
 
-		params := gin.LogFormatterParams{}
-		params.TimeStamp = time.Now()
-		params.Latency = params.TimeStamp.Sub(start)
-		params.ClientIP = c.ClientIP()
-		params.Method = c.Request.Method
-		params.StatusCode = c.Writer.Status()
-		params.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
-		params.BodySize = c.Writer.Size()
+			params := gin.LogFormatterParams{}
+			params.TimeStamp = time.Now()
+			params.Latency = params.TimeStamp.Sub(start)
+			params.ClientIP = c.ClientIP()
+			params.Method = c.Request.Method
+			params.StatusCode = c.Writer.Status()
+			params.ErrorMessage = c.Errors.ByType(gin.ErrorTypePrivate).String()
+			params.BodySize = c.Writer.Size()
 
-		if raw != "" {
-			path = path + "?" + raw
+			if raw != "" {
+				path = path + "?" + raw
+			}
+			params.Path = path
+
+			keys := map[logging.ExtraKey]interface{}{}
+			keys[logging.Path] = params.Path
+			keys[logging.ClientIp] = params.ClientIP
+			keys[logging.Method] = params.Method
+			keys[logging.Latency] = params.Latency
+			keys[logging.StatusCode] = params.StatusCode
+			keys[logging.ErrorMessage] = params.ErrorMessage
+			keys[logging.BodySize] = params.BodySize
+			keys[logging.RequestBody] = string(bodyBytes)
+			keys[logging.ResponseBody] = blw.body.String()
+
+			logger.Info(logging.RequestResponse, logging.Api, "", keys)
 		}
-		params.Path = path
-
-		keys := map[logging.ExtraKey]interface{}{}
-		keys[logging.Path] = params.Path
-		keys[logging.ClientIp] = params.ClientIP
-		keys[logging.Method] = params.Method
-		keys[logging.Latency] = params.Latency
-		keys[logging.StatusCode] = params.StatusCode
-		keys[logging.ErrorMessage] = params.ErrorMessage
-		keys[logging.BodySize] = params.BodySize
-		keys[logging.RequestBody] = string(bodyBytes)
-		keys[logging.ResponseBody] = blw.body.String()
-
-		logger.Info(logging.RequestResponse, logging.Api, "", keys)
 	}
 }
