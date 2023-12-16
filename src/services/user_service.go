72b4f34a27b16cd0de449d64e70dec341b7fd473
alireza-fa <alirezafeyze44@gmail.com>
2023-12-17 02:53:07 +0330

    update user_service(exists by mobile, username and email function)

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -4,7 +4,9 @@ import (
 	"github.com/alireza-fa/golang-car-shop/api/dto"
 	"github.com/alireza-fa/golang-car-shop/common"
 	"github.com/alireza-fa/golang-car-shop/config"
+	"github.com/alireza-fa/golang-car-shop/constants"
 	"github.com/alireza-fa/golang-car-shop/data/db"
+	"github.com/alireza-fa/golang-car-shop/data/models"
 	"github.com/alireza-fa/golang-car-shop/pkg/logging"
 	"gorm.io/gorm"
 )
@@ -34,4 +36,54 @@ func (s *UserService) SendOtp(req *dto.GetOtpRequest) error {
 		return err
 	}
 	return nil
+}
+
+func (s *UserService) existsByEmail(email string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("email = ?", email).
+		Find(&exists).Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByUsername(username string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(&models.User{}).
+		Select("count(*) > 0").
+		Where("username = ?", username).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) existsByMobileNumber(mobileNumber string) (bool, error) {
+	var exists bool
+	if err := s.database.Model(models.User{}).
+		Select("count(*) > 0").
+		Where("mobile_number = ?", mobileNumber).
+		Find(&exists).
+		Error; err != nil {
+		s.logger.Error(logging.Postgres, logging.Select, err.Error(), nil)
+		return false, err
+	}
+	return exists, nil
+}
+
+func (s *UserService) getDefaultRole() (roleId int, err error) {
+
+	if err = s.database.Model(&models.User{}).
+		Select("id").
+		Where("name = ?", constants.DefaultRoleName).
+		First(&roleId).
+		Error; err != nil {
+		return 0, err
+	}
+	return roleId, nil
 }
