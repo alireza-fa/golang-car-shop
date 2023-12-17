995915645e7b8e0e272346adcba78fced010bbc8
alireza-fa <alirezafeyze44@gmail.com>
2023-12-18 03:01:48 +0330

    add Authentication moddleware

---
+++ src/api/middlewares/auth.go
@@ -0,0 +1,53 @@
+package middlewares
+
+import (
+	"github.com/alireza-fa/golang-car-shop/api/helper"
+	"github.com/alireza-fa/golang-car-shop/config"
+	"github.com/alireza-fa/golang-car-shop/constants"
+	"github.com/alireza-fa/golang-car-shop/pkg/service_errors"
+	"github.com/alireza-fa/golang-car-shop/services"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+	"net/http"
+	"strings"
+)
+
+func Authentication(cfg *config.Config) gin.HandlerFunc {
+	var tokenService = services.NewTokenService(cfg)
+
+	return func(c *gin.Context) {
+		var err error
+		claimMap := map[string]interface{}{}
+		auth := c.GetHeader(constants.AuthorizationHeaderKey)
+		token := strings.Split(auth, " ")
+		if auth == "" {
+			err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenRequired}
+		} else {
+			claimMap, err = tokenService.GetClaims(token[1])
+			if err != nil {
+				switch err.(*jwt.ValidationError).Errors {
+				case jwt.ValidationErrorExpired:
+					err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenExpired}
+				default:
+					err = &service_errors.ServiceError{EndUserMessage: service_errors.TokenInvalid}
+				}
+			}
+		}
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized,
+				helper.GenerateBaseResponseWithError(nil, false, -2, err))
+			return
+		}
+
+		c.Set(constants.UserIdKey, claimMap[constants.UserIdKey])
+		c.Set(constants.FirstNameKey, claimMap[constants.FirstNameKey])
+		c.Set(constants.LastNameKey, claimMap[constants.LastNameKey])
+		c.Set(constants.UserNameKey, claimMap[constants.UserNameKey])
+		c.Set(constants.EmailKey, claimMap[constants.EmailKey])
+		c.Set(constants.MobileNumberKey, claimMap[constants.MobileNumberKey])
+		c.Set(constants.RolesKey, claimMap[constants.RolesKey])
+		c.Set(constants.ExpireTimeKey, claimMap[constants.ExpireTimeKey])
+
+		c.Next()
+	}
+}
