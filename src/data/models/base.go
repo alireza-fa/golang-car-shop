9b255dbc3096887664c506146935b30d529f6622
alireza-fa <alirezafeyze44@gmail.com>
2023-12-19 05:00:25 +0330

    add Country, City model

---
+++ src/data/models/base.go
@@ -0,0 +1,14 @@
+package models
+
+type Country struct {
+	BaseModel
+	Name   string `gorm:"size:10;type:string;not null;"`
+	cities []City
+}
+
+type City struct {
+	BaseModel
+	Name      `gorm:"size:10;type:string;not null"`
+	CountryId int
+	Country   Country `gorm:"foreignKey:CountryId;constraint:OnUpdate:NO ACTION;OnDelete:NO ACTION"`
+}
