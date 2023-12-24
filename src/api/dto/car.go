08f8c5ceff04e7eb7c96b656f551c71ddfeabaeb
alireza-fa <alirezafeyze44@gmail.com>
2023-12-24 06:12:56 +0330

    add car_model_price_history_service

--- src/api/dto/car.go
+++ src/api/dto/car.go
@@ -1,5 +1,7 @@
 package dto
 
+import "time"
+
 type CreateCarTypeRequest struct {
 	Name string `json:"name" binding:"required,alpha,min=3,max=15"`
 }
@@ -79,4 +81,22 @@ type CarModelYearResponse struct {
 	Id          int                            `json:"id"`
 	PersianYear PersianYearWithoutDateResponse `json:"persianYear,omitempty"`
 	CarModelId  int                            `json:"carModelId,omitempty"`
+}
+
+type CreateCarModelPriceHistoryRequest struct {
+	CarModelYearId int       `json:"carModelYearId" binding:"required"`
+	PriceAt        time.Time `json:"priceAt" binding:"required"`
+	Price          float64   `json:"price" binding:"required"`
+}
+
+type UpdateCarModelPriceHistoryRequest struct {
+	PriceAt time.Time `json:"priceAt,omitempty"`
+	Price   float64   `json:"price,omitempty"`
+}
+
+type CarModelPriceHistoryResponse struct {
+	Id             int       `json:"id"`
+	CarModelYearId int       `json:"carModelYearId"`
+	PriceAt        time.Time `json:"priceAt,omitempty"`
+	Price          float64   `json:"price,omitempty"`
 }
---
+++ src/services/car_model_price_history_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"context"
+	"github.com/alireza-fa/golang-car-shop/api/dto"
+	"github.com/alireza-fa/golang-car-shop/config"
+	"github.com/alireza-fa/golang-car-shop/data/db"
+	"github.com/alireza-fa/golang-car-shop/data/models"
+	"github.com/alireza-fa/golang-car-shop/pkg/logging"
+)
+
+type CarModelPriceHistoryService struct {
+	base *BaseService[models.CarModelPriceHistory, dto.CreateCarModelPriceHistoryRequest, dto.UpdateCarModelPriceHistoryRequest, dto.CarModelPriceHistoryResponse]
+}
+
+func NewCarModelPriceHistoryService(cfg *config.Config) *CarModelPriceHistoryService {
+	return &CarModelPriceHistoryService{
+		base: &BaseService[models.CarModelPriceHistory, dto.CreateCarModelPriceHistoryRequest, dto.UpdateCarModelPriceHistoryRequest, dto.CarModelPriceHistoryResponse]{
+			Database: db.GetDb(),
+			Logger:   logging.NewLogger(cfg),
+		},
+	}
+}
+
+// Create carModelPriceHistory
+func (s *CarModelPriceHistoryService) Create(ctx context.Context, req *dto.CreateCarModelPriceHistoryRequest) (*dto.CarModelPriceHistoryResponse, error) {
+	return s.base.Create(ctx, req)
+}
+
+// Update carModelPriceHistory
+func (s *CarModelPriceHistoryService) Update(ctx context.Context, id int, req *dto.UpdateCarModelPriceHistoryRequest) (*dto.CarModelPriceHistoryResponse, error) {
+	return s.base.Update(ctx, id, req)
+}
+
+// Delete carModelPriceHistory
+func (s *CarModelPriceHistoryService) Delete(ctx context.Context, id int) error {
+	return s.base.Delete(ctx, id)
+}
+
+// GetById carModelPriceHistory
+func (s *CarModelPriceHistoryService) GetById(ctx context.Context, id int) (*dto.CarModelPriceHistoryResponse, error) {
+	return s.base.GetById(ctx, id)
+}
+
+// GetByFilter carModelPriceHistory
+func (s *CarModelPriceHistoryService) GetByFilter(ctx context.Context, req *dto.PaginationInputWithFilter) (*dto.PagedList[dto.CarModelPriceHistoryResponse], error) {
+	return s.base.GetByFilter(ctx, req)
+}
